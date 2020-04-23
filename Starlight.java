4907371145f7bf434f468b2114ccdda991ecfd59
Bagus Muhamad Iqbal <63133969+bagusmuhamadiqbal@users.noreply.github.com>
2020-04-23 14:34:30 +0700

    Update Starlight.java

--- Starlight.java
+++ Starlight.java
@@ -5,15 +5,15 @@ public class Starlight {
     
     void hargaStandar(){
         System.out.println("Pesanan : Starlight Standar");
-        System.out.println("Total Bayar : Rp50000");
+        System.out.println("Total Bayar : Rp150000");
     }
     void hargaPlus(){
         System.out.println("Pesanan : Starlight Plus");
-        System.out.println("Total Bayar : Rp75000");
+        System.out.println("Total Bayar : Rp300000");
     }
     void hargaSeason(){
         System.out.println("Pesanan : Starlight Standar");
-        System.out.println("Total Bayar : Rp50000");
+        System.out.println("Total Bayar : Rp150000");
     }
     }
 
